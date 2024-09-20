7277fa270f9d559e6ea3df3c994a3b9dc9db4415
Markus Wüstenberg <markus@maragu.dk>
2024-09-20 11:15:35 +0200

    Use database on filesystem for tests
    The in-memory one kept making problems for me. I suspect because the DB would sometimes disappear, along with the migrations.

--- sqltest/helper.go
+++ sqltest/helper.go
@@ -2,6 +2,8 @@ package sqltest
 
 import (
 	"context"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/maragudk/goqite"
@@ -23,11 +25,16 @@ func (t *testWriter) Write(p []byte) (n int, err error) {
 func NewHelper(t *testing.T) *sql.Helper {
 	t.Helper()
 
+	cleanup(t)
+	t.Cleanup(func() {
+		cleanup(t)
+	})
+
 	sqlHelper := sql.NewHelper(sql.NewHelperOptions{
 		Log: snorkel.New(snorkel.Options{
 			W: &testWriter{t: t},
 		}),
-		Path: ":memory:",
+		Path: "test.db",
 	})
 	if err := sqlHelper.Connect(); err != nil {
 		t.Fatal(err)
@@ -44,4 +51,18 @@ func NewHelper(t *testing.T) *sql.Helper {
 	sqlHelper.SetJobsQueue(q)
 
 	return sqlHelper
+}
+
+func cleanup(t *testing.T) {
+	t.Helper()
+
+	files, err := filepath.Glob("test.db*")
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, file := range files {
+		if err := os.Remove(file); err != nil {
+			t.Fatal(err)
+		}
+	}
 }
