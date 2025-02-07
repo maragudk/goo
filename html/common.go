6b3f5ec2c72dcae8916ca8dc9d4e0f560e99a23a
Markus Wüstenberg <markus@maragu.dk>
2025-02-07 12:43:32 +0100

    Update FavIcons

--- html/common.go
+++ html/common.go
@@ -21,14 +21,24 @@ type PageProps struct {
 
 type PageFunc = func(props PageProps, children ...Node) Node
 
-func FavIcons(themeColor string) Node {
+func FavIcons(name, themeColor string) Node {
 	return Group([]Node{
+		// <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+		Link(Rel("icon"), Type("image/png"), Href("/favicon-96x96.png"), Attr("sizes", "96x96")),
+		// <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+		Link(Rel("icon"), Type("image/svg+xml"), Href("/favicon.svg")),
+
+		// <link rel="shortcut icon" href="/favicon.ico" />
+		Link(Rel("shortcut icon"), Href("/favicon.ico")),
+
+		// <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
 		Link(Rel("apple-touch-icon"), Attr("sizes", "180x180"), Href("/apple-touch-icon.png")),
-		Link(Rel("icon"), Type("image/png"), Attr("sizes", "32x32"), Href("/favicon-32x32.png")),
-		Link(Rel("icon"), Type("image/png"), Attr("sizes", "16x16"), Href("/favicon-16x16.png")),
+
+		// <meta name="apple-mobile-web-app-title" content="name" />
+		Meta(Name("apple-mobile-web-app-title"), Content(name)),
+
+		// <link rel="manifest" href="/site.webmanifest" />
 		Link(Rel("manifest"), Href("/manifest.json")),
-		Meta(Name("msapplication-TileColor"), Content(themeColor)),
-		Meta(Name("theme-color"), Content(themeColor)),
 	})
 }
 
